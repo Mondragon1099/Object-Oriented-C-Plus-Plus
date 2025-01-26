f8e11be8e43cd0dfc802a6552d5ea9fc89c2d72a
Mondragon <Mondragon1099@hotmail.com>
2025-01-26 00:07:34 -0800

    added Hello.cpp
    first program !

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,14 @@
+/*
+This is to output hello world in VSCode while also learning version
+control through Github Desktop.
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
